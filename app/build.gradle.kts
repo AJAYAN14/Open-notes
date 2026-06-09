1364d4f7583905cc6982be63ad26e6391de7b307
Fandroid745 <dhanush41230@gmail.com>
2026-06-09 23:27:45 +0530

    feat: add libs version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
+
+
+
 android {
     namespace = "com.opennotes"
     compileSdk = 36
@@ -61,50 +64,40 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2025.05.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.01"))
-    implementation("com.materialkolor:material-kolor:2.0.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    implementation("androidx.datastore:datastore-core-android:1.2.1")
-    implementation("androidx.datastore:datastore-preferences:1.2.1")
-
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
-
-    implementation("com.google.dagger:hilt-android:2.56")
-    ksp("com.google.dagger:hilt-android-compiler:2.56")
-    ksp("androidx.hilt:hilt-compiler:1.2.0")
-
-    implementation("androidx.room:room-runtime:2.7.0")
-    implementation("androidx.room:room-ktx:2.7.0")
-    ksp("androidx.room:room-compiler:2.7.0")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-
-    implementation("com.google.code.gson:gson:2.11.0")
-
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.biometric:biometric:1.1.0")
-
-    implementation("androidx.glance:glance-appwidget:1.1.1")
-    implementation("androidx.glance:glance-material3:1.1.1")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material.icons.extended)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    debugImplementation(libs.compose.ui.tooling)
+    androidTestImplementation(platform(libs.compose.bom))
+    implementation(libs.material.kolor)
+    androidTestImplementation(libs.compose.test.junit)
+    implementation(libs.androidx.datastore.core)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.activity.compose)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.hilt.compiler)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    implementation(libs.gson)
+    implementation(libs.core.splashscreen)
+    implementation(libs.biometric)
+    implementation(libs.glance.appwidget)
+    implementation(libs.glance.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.espresso)
 }
